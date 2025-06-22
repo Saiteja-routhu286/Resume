986028f7bc638ea4d383bc092acf7c5e805ddb01
Saiteja-routhu286 <sai826636@gmail.com>
2025-06-22 17:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("themeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
+
+      document.body.classList.toggle("dark-mode");
+
+    });
+
+    function showSkill(name, percent) {
+
+      const display = document.getElementById("skillDisplay");
+
+      display.textContent = `${name} — ${percent}`;
+
+    }
+
+ 
